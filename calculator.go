915144cfdda3d6c48d8f1bd363726291e62914fa
Charlie Tekulve <54796991+chatek87@users.noreply.github.com>
2024-04-26 16:44:29 -0400

    flesh out models, calculator

--- calculator.go
+++ calculator.go
@@ -1,26 +1,55 @@
 package main
 
 type Calculator struct {
-	BarTeam BarTeam
-	Servers []Server
-	Events  []Event
-	SupportTeam []Support
+	// input fields
+	BarTeamIn     BarTeamIn
+	ServersIn     []ServerIn
+	EventsIn      []EventIn
+	SupportTeamIn []SupportIn
 
-	TotalBarHours float64
-	TotalSupportHours float64
+	// output fields
+	BarTeamOut     BarTeamOut
+	ServersOut     []ServerOut
+	EventsOut      []EventOut
+	SupportTeamOut []SupportOut
 }
 
-func (c *Calculator) SetTotalBarHours() {
+func (c *Calculator) GetTotalBarHours() float64 {
 	totalHours := 0.0
-	for _, bartender := range c.BarTeam.Bartenders {
+	for _, bartender := range c.BarTeamIn.Bartenders {
 		totalHours += bartender.Hours
 	}
-	c.TotalBarHours = totalHours
+	return totalHours
 }
 
-func (c *Calculator) SetTotalSupportHours() {
+func (c *Calculator) GetTotalSupportHours() float64 {
 	totalHours := 0.0
-	for _, support := range c.SupportTeam {
+	for _, support := range c.SupportTeamIn {
 		totalHours += support.Hours
+	}
+	return totalHours
+}
+
+func (c *Calculator) GetTipoutPercentageToBar() float64 {
+	supportCount := len(c.SupportTeamIn)
+	if supportCount == 0 {
+		return 0.00
+	}
+	if supportCount >= 3 {
+		return 0.015
+	} else {
+		return 0.02
+	}
+}
+
+func (c *Calculator) GetTipoutPercentageToSupport() float64 {
+	supportCount := len(c.SupportTeamIn)
+	if supportCount == 0 {
+		return 0.00
+	}
+	if supportCount <= 3 {
+		return float64(supportCount) * 0.01
+	} else {
+		return 0.03
 	}
 }
--- models.go
+++ models.go
@@ -1,30 +1,80 @@
 package main
 
-type Bartender struct {
+// input models
+type BartenderIn struct {
 	Name  string
 	Hours float64
 }
 
-type BarTeam struct {
-	Bartenders []Bartender
+type BarTeamIn struct {
+	Bartenders []BartenderIn
 	OwedTo     float64
 	Sales      float64
 }
 
-type Server struct {
+type ServerIn struct {
 	Name   string
 	OwedTo float64
 	Sales  float64
 }
 
-type Event struct {
+type EventIn struct {
 	Name    string
 	OwedTo  float64
 	Sales   float64
 	SplitBy int
 }
 
-type Support struct {
+type SupportIn struct {
 	Name  string
 	Hours float64
+}
+
+// output models ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+type BartenderOut struct {
+	Name                 string
+	Hours                float64
+	OwedToPreTipout      float64
+	TipoutToSupport      float64
+	TotalAmountTippedOut float64
+	FinalPayout          float64
+}
+
+type BarTeamOut struct {
+	Bartenders           []BartenderOut
+	OwedToPreTipout      float64
+	Sales                float64
+	TipoutToSupport      float64
+	TotalAmountTippedOut float64
+	FinalPayout          float64
+}
+
+type ServerOut struct {
+	Name                 string
+	OwedToPreTipout      float64
+	Sales                float64
+	TipoutToBar          float64
+	TipoutToSupport      float64
+	TotalAmountTippedOut float64
+	FinalPayout          float64
+}
+
+type EventOut struct {
+	Name                 string
+	OwedToPreTipout      float64
+	Sales                float64
+	TipoutToBar          float64
+	TipoutToSupport      float64
+	TotalAmountTippedOut float64
+	FinalPayout          float64
+	SplitBy              int
+}
+
+type SupportOut struct {
+	Name              string
+	Hours             float64
+	TipoutFromBar     float64
+	TipoutFromServers float64
+	TipoutFromEvents  float64
+	FinalPayout       float64
 }
